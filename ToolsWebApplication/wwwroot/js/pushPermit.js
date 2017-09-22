7984afb0fa9291c745590aea7b655b1caed0c1ad
nakamura <g.nakamura@ccs-kk.co.jp>
2017-09-22 10:35:59 +0900

    add: serviceWorker , robots.txt ,manifest.json , pushPermit cosme: _Layout.cshtml

---
+++ ToolsWebApplication/wwwroot/js/pushPermit.js
@@ -0,0 +1,23 @@
+﻿// Write your JavaScript code.
+
+//プッシュ通知の許可を聞くjs 
+window.addEventListener('load', function () {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/serviceWorker.js')
+            .then(function (registration) {
+                return registration.pushManager.getSubscription().then(function (subscription) {
+                    if (subscription) {
+                        return subscription
+                    }
+                    return registration.pushManager.subscribe({
+                        userVisibleOnly: true
+                    })
+                })
+            }).then(function (subscription) {
+                var endpoint = subscription.endpoint
+                console.log("pushManager endpoint:", endpoint) // https://android.googleapis.com/gcm/send/******:******......
+            }).catch(function (error) {
+                console.warn("serviceWorker error:", error)
+            })
+    }
+})
---
+++ ToolsWebApplication/wwwroot/serviceWorker.js
@@ -0,0 +1,86 @@
+'use strict';
+
+// キャッシュに保存するときの名前
+const CACHE_NAME = 'cache-v1';
+const urlsToCache = [
+    './',
+    './css/site.css',
+    './js/image.jpg',
+    './script/main.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+
+                // 指定されたリソースをキャッシュに追加する
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    var cacheWhitelist = [CACHE_NAME];
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    // ホワイトリストにないキャッシュ(古いキャッシュ)は削除する
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+
+                // 重要：リクエストを clone する。リクエストは Stream なので
+                // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+                // 必要なので、リクエストは clone しないといけない
+                let fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest)
+                    .then((response) => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // 重要：レスポンスを clone する。レスポンスは Stream で
+                        // ブラウザ用とキャッシュ用の2回必要。なので clone して
+                        // 2つの Stream があるようにする
+                        let responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    });
+            })
+    );
+});
+
+self.addEventListener('push', e => {
+    console.log('Service Worker onpush: ', e);
+
+    e.waitUntil(
+        self.registration.showNotification('Push Notification Title', {
+            body: '(・∀・)',
+            icon: 'http://placehold.it/192x192',
+            tag: 'push-notification-tag'
+        })
+    );
+});
